5fc5a7a89b6c2076461ef6b4a0e92b1ea236dff4
huageyiyangdewo <1981097256@qq.com>
2020-07-01 22:28:36 +0800

    第二章--2.2--感知器

---
+++ chapter_1/perceptron.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+class Perceptron(object):
+    '''
+    面向对象的感知器API
+    '''
+
+    def __init__(self, eta=0.0, n_iter=50, random_state=1):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        # size+1是创建x(0)，方便计算
+        regn = np.random.RandomState(self.random_state)
+
+        self.w_ = regn.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+            errors = 0
+            for xi, target in zip(X, y):
+                # 更新的权重
+                update = self.eta * (target - self.predict(xi))
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+                errors += int(update != 0)  # 分类错误的数量
+
+            self.errors_.append(errors)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:] + self.w_[0])
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
