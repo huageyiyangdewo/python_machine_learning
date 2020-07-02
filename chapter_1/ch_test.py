b7b2870e046366ae7b8228a2e3d88cdf0ecd0699
huageyiyangdewo <1981097256@qq.com>
2020-07-02 22:15:10 +0800

    感知器决策边界

---
+++ chapter_1/ch_test.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+
+from chapter_1.perceptron import Perceptron
+
+
+class OvATest(object):
+
+    def __init__(self, url='https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'):
+        self.url = url
+        self.df = None
+        self.y = None
+        self.X = None
+        self.init_data()
+
+    def init_data(self):
+        df = pd.read_csv(self.url, header=None)
+        self.df = df
+        y = self.df.iloc[0:100, 4].values
+        y = np.where(y == 'Iris-setosa', -1, 1)
+
+        X = self.df.iloc[0:100, [0, 2]].values
+        self.y = y
+        self.X = X
+
+    def visual_data(self):
+
+        plt.scatter(self.X[:50, 0], self.X[:50, 1], color='red', marker='o', label='setosa')
+        plt.scatter(self.X[50:100, 0], self.X[50:100, 1], color='blue', marker='x', label='versicolor')
+        plt.xlabel('sepal length [cm]')
+        plt.ylabel('peta; length [cm]')
+        plt.legend(loc='upper left')
+        plt.show()
+
+
+def test_perceptron(X, y):
+    ppn = Perceptron(eta=0.1, n_iter=10)
+    ppn.fit(X, y)
+    plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+    plt.xlabel('Epochas')
+    plt.ylabel('Number of updates')
+    plt.show()
+    return ppn
+
+
+def plot_decision_region(X, y, classifier, resolution=0.02):
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() - 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() - 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution)
+                           )
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, c1 in enumerate(np.unique(y)):
+        plt.scatter(
+            x=X[y == c1, 0],
+            y=X[y == c1, 1],
+            alpha=0.8,
+            c=colors[idx],
+            marker=markers[idx],
+            label=c1,
+            edgecolors='black'
+        )
+
+
+def visual_ret(X, y, ppn):
+    plot_decision_region(X, y, classifier=ppn)
+    plt.xlabel('sepal length [cm]')
+    plt.ylabel('petal length [cm]')
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+if __name__ == '__main__':
+    t = OvATest()
+    # t.visual_data()
+    ppn = test_perceptron(t.X, t.y)
+    visual_ret(t.X, t.y, ppn)
